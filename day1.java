79a1a7961e43980cfa0ef6f0ab06c41a612ecd84
Sivaramalakshmi <sivaramalakshmi545@gmail.com>
2026-02-25 21:14:13 +0530

    Intro & Print Statements

---
+++ day1.java
@@ -0,0 +1,27 @@
+package Java_Practice;
+
+// Program - Instructions to computer to perform a specific task.
+// JAva - Popular programming language used for developing applications, web applications, mobile apps, and more.
+// Compiler - A program that translates source code into machine code that can be executed by a computer.
+// Source -> Compiler -> Byte code -> JVM (Interpreter) -> Processor
+// Methods ()- A block of code that performs a specific task and can be called upon when needed. 
+// Classes - A blueprint for creating objects that encapsulate data and behavior.It has data and methods.
+// Packages - A way to organize classes and interfaces into namespaces, providing a unique namespace for each type it contains.( Eg. java.util, java.io, java.lang)
+
+public class day1 {
+    
+    public static void main(String[] args) {
+
+        // public - Access modifier that allows the class to be accessed from anywhere
+        // static - No need to create object.Just run it directly.
+        // main - The entry point of a Java application. It is the method that is executed when the program is run.
+        // String[] args - An array of strings that can be used to pass command-line arguments to the program.
+
+
+        // Print 
+        System.out.println("Hello, World!"); 
+        System.out.println("Welcome to Java Programming!"); // Print with new line
+    }
+
+
+}
